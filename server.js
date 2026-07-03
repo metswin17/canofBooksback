be8fbca7b0e94e6e20c6e1e85131e9d98122536f
Luis Lopez <metfanforever@gmail.com>
2026-07-02 23:53:55 -0700

    mod server

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 
 const PORT = 3001;
 
@@ -21,6 +23,15 @@ app.get('/', (req, res) => {
 app.get('/books', async (req, res) => {
   const books = await Book.find({});
   res.json(books);
+});
+
+app.post('/books', async (req, res) => {
+  try {
+    const newBook = await Book.create(req.body);
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 app.listen(PORT, () => {
